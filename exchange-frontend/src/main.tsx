a131e8b3691fa9e9a8c661207975b54a8026db1c
Sean Lau <lthon0225@gmail.com>
2022-05-17 12:11:17 +0800

    fix: downgrade to react 17

--- exchange-frontend/src/main.tsx
+++ exchange-frontend/src/main.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from 'react-dom'
 import './index.css'
+import App from './App'
 import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from 'react-query'
-
 const queryClient = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
@@ -15,4 +14,23 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>,
+  document.getElementById('root'),
 )
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
+// import { ChakraProvider } from '@chakra-ui/react'
+// import { QueryClient, QueryClientProvider } from 'react-query'
+
+// const queryClient = new QueryClient()
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <QueryClientProvider client={queryClient}>
+//       <ChakraProvider>
+//         <App />
+//       </ChakraProvider>
+//     </QueryClientProvider>
+//   </React.StrictMode>,
+// )
